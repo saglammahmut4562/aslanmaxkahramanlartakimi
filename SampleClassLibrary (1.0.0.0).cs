7b69adf4c91dc10ce66ff04a4cedb2eaeb094efe
Mahmut Saglam <saglam.mahmut4562@gmail.com>
2026-08-02 18:50:58 +0300

    Add files via upload

---
+++ SampleClassLibrary (1.0.0.0).cs
@@ -0,0 +1,25 @@
+// C:\Users\RAMPAGE\OneDrive\Desktop\assets\bin\Data\Managed\SampleClassLibrary.dll
+// SampleClassLibrary, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+
+// Zaman Bilgisi: 53DF9DA5 (4.08.2014 17:50:13)
+
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTitle("SampleClassLibrary")]
+[assembly: AssemblyDescription("")]
+[assembly: ComVisible(false)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: CompilationRelaxations(8)]
+[assembly: Guid("178cf652-c764-49c2-bd8f-329fd5b831b2")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyProduct("SampleClassLibrary")]
+[assembly: AssemblyCompany("")]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
---
+++ UnityEngine (0.0.0.0).cs
@@ -0,0 +1,11 @@
+// C:\Users\RAMPAGE\OneDrive\Desktop\assets\bin\Data\Managed\UnityEngine.dll
+// UnityEngine, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+
+// Zaman Bilgisi: 53DF9DA4 (4.08.2014 17:50:12)
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
